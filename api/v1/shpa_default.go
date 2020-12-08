6992648e912b44bb819c336a85465407083ec36f
Yongqi Feng <fyqnankai@163.com>
2020-12-08 16:33:20 +0800

    watermark

---
+++ api/v1/shpa_default.go
@@ -0,0 +1,148 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+package v1
+
+import "fmt"
+
+const (
+	defaultTolerance                       = 0.1
+	defaultDownscaleForbiddenWindowSeconds = 300
+	defaultUpscaleForbiddenWindowSeconds   = 60
+	defaultScaleDownLimitFactor            = 20
+	defaultScaleUpLimitFactor              = 50
+	// Most common use case is to autoscale over avg:kubernetes.cpu.usage, which directly correlates to the # replicas.
+	defaultAlgorithm         = "absolute"
+	defaultMinReplicas int32 = 1
+)
+
+func DefaultSHPA(shpa *SHPA) *SHPA {
+	defaultSHPA := shpa.DeepCopy()
+
+	if shpa.Spec.MinReplicas == 0 {
+		defaultSHPA.Spec.MinReplicas = defaultMinReplicas
+	}
+	if shpa.Spec.Algorithm == ""{
+		defaultSHPA.Spec.Algorithm = defaultAlgorithm
+	}
+	if shpa.Spec.Tolerance == 0 {
+		defaultSHPA.Spec.Tolerance = defaultTolerance
+	}
+	if shpa.Spec.ScaleUpLimitFactor == 0 {
+		defaultSHPA.Spec.ScaleUpLimitFactor = defaultScaleUpLimitFactor
+	}
+	if shpa.Spec.ScaleDownLimitFactor == 0 {
+		defaultSHPA.Spec.ScaleDownLimitFactor = defaultScaleDownLimitFactor
+	}
+	if shpa.Spec.DownscaleForbiddenWindowSeconds == 0 {
+		defaultSHPA.Spec.DownscaleForbiddenWindowSeconds = defaultDownscaleForbiddenWindowSeconds
+	}
+	if shpa.Spec.UpscaleForbiddenWindowSeconds == 0 {
+		defaultSHPA.Spec.UpscaleForbiddenWindowSeconds = defaultUpscaleForbiddenWindowSeconds
+	}
+
+	return defaultSHPA
+}
+
+// IsDefaultWatermarkPodAutoscaler used to know if a WatermarkPodAutoscaler has default values
+func IsDefaultSHPA(spa *SHPA) bool {
+
+	if spa.Spec.MinReplicas == 0 {
+		return false
+	}
+	if spa.Spec.Algorithm == "" {
+		return false
+	}
+	if spa.Spec.Tolerance == 0 {
+		return false
+	}
+	if spa.Spec.ScaleUpLimitFactor == 0 {
+		return false
+	}
+	if spa.Spec.ScaleDownLimitFactor == 0 {
+		return false
+	}
+	if spa.Spec.DownscaleForbiddenWindowSeconds == 0 {
+		return false
+	}
+	if spa.Spec.UpscaleForbiddenWindowSeconds == 0 {
+		return false
+	}
+	return true
+}
+
+
+// CheckSHPAValidity use to check the validty of a SHPA
+// return nil if valid, else an error
+func CheckSHPAValidity(shpa *SHPA) error {
+	if shpa.Spec.ScaleTargetRef.Kind == "" || shpa.Spec.ScaleTargetRef.Name == "" {
+		msg := fmt.Sprintf("the Spec.ScaleTargetRef should be populated, currently Kind:%s and/or Name:%s are not set properly", shpa.Spec.ScaleTargetRef.Kind, shpa.Spec.ScaleTargetRef.Name)
+		return fmt.Errorf(msg)
+	}
+	if shpa.Spec.MinReplicas == 0 || shpa.Spec.MaxReplicas < shpa.Spec.MinReplicas {
+		msg := fmt.Sprintf("watermark pod autoscaler requires the minimum number of replicas to be configured and inferior to the maximum")
+		return fmt.Errorf(msg)
+	}
+	return checkSPAMetricsValidity(shpa)
+}
+
+func checkSPAMetricsValidity(shpa *SHPA) (err error) {
+	// This function will not be needed for the vanilla k8s.
+	// For now we check only nil pointers here as they crash the default controller algorithm
+	// We also make sure that the Watermarks are properly set.
+	for _, metric := range shpa.Spec.Metrics {
+		switch metric.Type {
+		case "External":
+			if metric.External == nil {
+				return fmt.Errorf("metric.External is nil while metric.Type is '%s'", metric.Type)
+			}
+			if metric.External.LowWatermark == nil || metric.External.HighWatermark == nil {
+				msg := fmt.Sprintf("Watermarks are not set correctly, removing the WPA %s/%s from the Reconciler", shpa.Namespace, shpa.Name)
+				return fmt.Errorf(msg)
+			}
+			if metric.External.MetricSelector == nil {
+				msg := fmt.Sprintf("Missing Labels for the External metric %s", metric.External.MetricName)
+				return fmt.Errorf(msg)
+			}
+			if metric.External.HighWatermark.MilliValue() < metric.External.LowWatermark.MilliValue() {
+				msg := fmt.Sprintf("Low WaterMark of External metric %s{%s} has to be strictly inferior to the High Watermark", metric.External.MetricName, metric.External.MetricSelector.MatchLabels)
+				return fmt.Errorf(msg)
+			}
+		case "Resource":
+			if metric.Resource == nil {
+				return fmt.Errorf("metric.Resource is nil while metric.Type is '%s'", metric.Type)
+			}
+			if metric.Resource.LowWatermark == nil || metric.Resource.HighWatermark == nil {
+				msg := fmt.Sprintf("Watermarks are not set correctly, removing the WPA %s/%s from the Reconciler", shpa.Namespace, shpa.Name)
+				return fmt.Errorf(msg)
+			}
+			if metric.Resource.MetricSelector == nil {
+				msg := fmt.Sprintf("Missing Labels for the Resource metric %s", metric.Resource.Name)
+				return fmt.Errorf(msg)
+			}
+			if metric.Resource.HighWatermark.MilliValue() < metric.Resource.LowWatermark.MilliValue() {
+				msg := fmt.Sprintf("Low WaterMark of Resource metric %s{%s} has to be strictly inferior to the High Watermark", metric.Resource.Name, metric.Resource.MetricSelector.MatchLabels)
+				return fmt.Errorf(msg)
+			}
+		default:
+			return fmt.Errorf("incorrect metric.Type: '%s'", metric.Type)
+		}
+	}
+	return err
+}
+
+
--- api/v1/shpa_types.go
+++ api/v1/shpa_types.go
@@ -17,29 +17,31 @@ limitations under the License.
 package v1
 
 import (
+	autoscalingv2 "k8s.io/api/autoscaling/v2beta1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// SHPASpec defines the desired state of SHPA
-type SHPASpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of SHPA. Edit SHPA_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-}
 
-// SHPAStatus defines the observed state of SHPA
-type SHPAStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-}
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// WatermarkPodAutoscaler is the Schema for the watermarkpodautoscalers API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="value",type="string",JSONPath=".status.currentMetrics[*].external.currentValue.."
+// +kubebuilder:printcolumn:name="high watermark",type="string",JSONPath=".spec.metrics[*].external.highWatermark.."
+// +kubebuilder:printcolumn:name="low watermark",type="string",JSONPath=".spec.metrics[*].external.lowWatermark.."
+// +kubebuilder:printcolumn:name="age",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="min replicas",type="integer",JSONPath=".spec.minReplicas"
+// +kubebuilder:printcolumn:name="max replicas",type="integer",JSONPath=".spec.maxReplicas"
+// +kubebuilder:printcolumn:name="dry-run",type="string",JSONPath=".spec.dryRun"
 // +kubebuilder:object:root=true
-
 // SHPA is the Schema for the shpas API
 type SHPA struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -49,14 +51,166 @@ type SHPA struct {
 	Status SHPAStatus `json:"status,omitempty"`
 }
 
-// +kubebuilder:object:root=true
+// SHPASpec defines the desired state of SHPA
+type SHPASpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// part of HorizontalController, see comments in the k8s repo: pkg/controller/podautoscaler/horizontal.go
+	// +kubebuilder:validation:Minimum=1
+	DownscaleForbiddenWindowSeconds int32 `json:"downscaleForbiddenWindowSeconds,omitempty"`
 
+	// +kubebuilder:validation:Minimum=1
+	UpscaleForbiddenWindowSeconds int32 `json:"upscaleForbiddenWindowSeconds,omitempty"`
+
+	// Percentage of replicas that can be added in an upscale event. Max value will set the limit at the Maximum number of Replicas.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
+	ScaleUpLimitFactor float64 `json:"scaleUpLimitFactor,omitempty"`
+
+	// Percentage of replicas that can be added in an upscale event. Max value will set the limit at the Maximum number of Replicas.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=100
+	ScaleDownLimitFactor float64 `json:"scaleDownLimitFactor,omitempty"`
+
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:ExclusiveMinimum=true
+	// +kubebuilder:validation:Maximum=1
+	// +kubebuilder:validation:ExclusiveMaximum=true
+	Tolerance float64 `json:"tolerance,omitempty"`
+
+	// computed values take the # of replicas into account
+	Algorithm string `json:"algorithm,omitempty"`
+
+	// Whether planned scale changes are actually applied
+	DryRun bool `json:"dryRun,omitempty"`
+
+	// part of HorizontalPodAutoscalerSpec, see comments in the k8s-1.10.8 repo: staging/src/k8s.io/api/autoscaling/v1/types.go
+	// reference to scaled resource; horizontal pod autoscaler will learn the current resource consumption
+	// and will set the desired number of pods by using its Scale subresource.
+	ScaleTargetRef CrossVersionObjectReference `json:"scaleTargetRef"`
+	// specifications that will be used to calculate the desired replica count
+	// +listType=set
+	Metrics []MetricSpec `json:"metrics,omitempty"`
+	// +kubebuilder:validation:Minimum=1
+	MinReplicas int32 `json:"minReplicas,omitempty"`
+	// +kubebuilder:validation:Minimum=1
+	MaxReplicas int32 `json:"maxReplicas,omitempty"`
+	// +kubebuilder:validation:Minimum=1
+	ReadinessDelaySeconds int32 `json:"readinessDelay,omitempty"`
+}
+
+// SHPAStatus defines the observed state of SHPA
+type SHPAStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	ObservedGeneration *int64       `json:"observedGeneration,omitempty"`
+	LastScaleTime      *metav1.Time `json:"lastScaleTime,omitempty"`
+	CurrentReplicas    int32        `json:"currentReplicas"`
+	DesiredReplicas    int32        `json:"desiredReplicas"`
+	// +listType=set
+	CurrentMetrics []autoscalingv2.MetricStatus `json:"currentMetrics"`
+	// +listType=set
+	Conditions []autoscalingv2.HorizontalPodAutoscalerCondition `json:"conditions"`
+}
+
+// +kubebuilder:object:root=true
+// CrossVersionObjectReference contains enough information to let you identify the referred resource.
+// +k8s:openapi-gen=true
+type CrossVersionObjectReference struct {
+	// Kind of the referent; More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds"
+	Kind string `json:"kind"`
+	// Name of the referent; More info: http://kubernetes.io/docs/user-guide/identifiers#names
+	Name string `json:"name"`
+	// API version of the referent
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+}
 // SHPAList contains a list of SHPA
 type SHPAList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []SHPA `json:"items"`
 }
+
+// ExternalMetricSource indicates how to scale on a metric not associated with
+// any Kubernetes object (for example length of queue in cloud
+// messaging service, or QPS from loadbalancer running outside of cluster).
+// Exactly one "target" type should be set.
+// +k8s:openapi-gen=true
+type ExternalMetricSource struct {
+	// metricName is the name of the metric in question.
+	MetricName string `json:"metricName"`
+	// metricSelector is used to identify a specific time series
+	// within a given metric.
+	// +optional
+	MetricSelector *metav1.LabelSelector `json:"metricSelector,omitempty"`
+
+	HighWatermark *resource.Quantity `json:"highWatermark,omitempty"`
+	LowWatermark  *resource.Quantity `json:"lowWatermark,omitempty"`
+}
+
+// ResourceMetricSource indicates how to scale on a resource metric known to
+// Kubernetes, as specified in requests and limits, describing each pod in the
+// current scale target (e.g. CPU or memory).  The values will be averaged
+// together before being compared to the target.  Such metrics are built in to
+// Kubernetes, and have special scaling options on top of those available to
+// normal per-pod metrics using the "pods" source.  Only one "target" type
+// should be set.
+// +k8s:openapi-gen=true
+type ResourceMetricSource struct {
+	// name is the name of the resource in question.
+	Name v1.ResourceName `json:"name"`
+	// metricSelector is used to identify a specific time series
+	// within a given metric.
+	// +optional
+	MetricSelector *metav1.LabelSelector `json:"metricSelector,omitempty"`
+
+	HighWatermark *resource.Quantity `json:"highWatermark,omitempty"`
+	LowWatermark  *resource.Quantity `json:"lowWatermark,omitempty"`
+}
+
+// MetricSourceType indicates the type of metric.
+type MetricSourceType string
+
+var (
+	// ExternalMetricSourceType is a global metric that is not associated
+	// with any Kubernetes object. It allows autoscaling based on information
+	// coming from components running outside of cluster
+	// (for example length of queue in cloud messaging service, or
+	// QPS from loadbalancer running outside of cluster).
+	ExternalMetricSourceType MetricSourceType = "External"
+
+	// ResourceMetricSourceType is a resource metric known to Kubernetes, as
+	// specified in requests and limits, describing each pod in the current
+	// scale target (e.g. CPU or memory).  Such metrics are built in to
+	// Kubernetes, and have special scaling options on top of those available
+	// to normal per-pod metrics (the "pods" source).
+	ResourceMetricSourceType MetricSourceType = "Resource"
+)
+
+// MetricSpec specifies how to scale based on a single metric
+// (only `type` and one other matching field should be set at once).
+// +k8s:openapi-gen=true
+type MetricSpec struct {
+	// type is the type of metric source.  It should be one of "Object",
+	// "Pods" or "Resource", each mapping to a matching field in the object.
+	Type MetricSourceType `json:"type"`
+	// external refers to a global metric that is not associated
+	// with any Kubernetes object. It allows autoscaling based on information
+	// coming from components running outside of cluster
+	// (for example length of queue in cloud messaging service, or
+	// QPS from loadbalancer running outside of cluster).
+	// +optional
+	External *ExternalMetricSource `json:"external,omitempty"`
+	// resource refers to a resource metric (such as those specified in
+	// requests and limits) known to Kubernetes describing each pod in the
+	// current scale target (e.g. CPU or memory). Such metrics are built in to
+	// Kubernetes, and have special scaling options on top of those available
+	// to normal per-pod metrics using the "pods" source.
+	// +optional
+	Resource *ResourceMetricSource `json:"resource,omitempty"`
+}
+
 
 func init() {
 	SchemeBuilder.Register(&SHPA{}, &SHPAList{})
