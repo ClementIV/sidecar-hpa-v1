a2802a40d564e66f8955eac1feb19ea1e4e2ca11
Yongqi Feng <fyqnankai@163.com>
2020-12-25 20:25:08 +0800

    add log

--- main.go
+++ main.go
@@ -18,13 +18,10 @@ package main
 
 import (
 	"flag"
-	"github.com/spf13/pflag"
 	"os"
-	"sidecar-hpa/version"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/restmapper"
 	shpav1 "sidecar-hpa/api/v1"
@@ -36,7 +33,6 @@ import (
 
 // Change below variables to serve metrics on different host or port.
 var log = logf.Log.WithName("cmd")
-var printVersionArg bool
 
 func main() {
 	// Add the zap logger flag set to the CLI. The flag set must
@@ -47,53 +43,18 @@ func main() {
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.Parse()
 
-	pflag.CommandLine.AddFlagSet(zap.FlagSet())
-
-	// Add flags registered by imported packages (e.g. glog and
-	// controller-runtime)
-	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-	pflag.BoolVarP(&printVersionArg, "version", "v", printVersionArg, "print version")
-	pflag.Parse()
-
-	// Use a zap logr.Logger implementation. If none of the zap
-	// flags are configured (or if the zap flag set is not being
-	// used), this defaults to a production zap logger.
-	//
-	// The logger instantiated here can be changed to any logger
-	// implementing the logr.Logger interface. This logger will
-	// be propagated through the whole operator, generating
-	// uniform and structured logs.
 	logf.SetLogger(zap.Logger())
-
-	if printVersionArg {
-		version.PrintVersionWriter(os.Stdout)
-		os.Exit(0)
-	}
-
-	version.PrintVersionLogs(log)
-
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
-		os.Exit(1)
-	}
-	log.Info("after get namespace")
-	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
 	log.Info("after get config")
-
 	// Become the leader before proceeding
-	log.Info("shpa-lock before")
-
-	log.Info("shpa-lock end")
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
-		Namespace:          namespace,
 		MapperProvider:     restmapper.NewDynamicRESTMapper,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
